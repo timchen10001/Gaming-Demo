02a99d35732e9bf843e8a8a6ea4b28edf5dbe08a
timchen <timchen10001@gmail.com>
2021-11-20 23:04:05 +0800

    fix(cube): modified cube component

--- app/src/ui/Cube/index.jsx
+++ app/src/ui/Cube/index.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-indent-props */
 import React from 'react';
 import T from 'prop-types';
 import { useBox } from 'use-cannon';
+import * as textures from '@@constants/textures';
 
 export const Cube = ({
   position,
@@ -17,6 +20,12 @@ export const Cube = ({
     <mesh
       castShadow
       ref={ref}>
+      {[...Array(6)].map((_, index) => (
+        <meshStandardMaterial
+          key={index}
+          attachArray="material"
+          map={textures[type]} />
+      ))}
       <boxBufferGeometry
         attach="geometry" />
     </mesh>
@@ -24,7 +33,11 @@ export const Cube = ({
 };
 
 Cube.propTypes = {
-  position: T.arrayOf().isRequired,
+  position: T.arrayOf(T.shape({
+    x: T.number.isRequired,
+    y: T.number.isRequired,
+    z: T.number.isRequired,
+  }).isRequired).isRequired,
   type: T.string.isRequired,
 };
 
